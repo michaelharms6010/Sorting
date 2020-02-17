bbd34261af5392d88698d05b269b02847edc4121
Michael Harms <michael.harms6010@gmail.com>
2020-02-17 10:10:39 -0600

    formats and removes reduntant initialization

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,27 +4,47 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
-
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]      
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    swapped = True
+    while swapped:
+        swapped = False
+        for index in range(len(arr)):
+            if index == 0:
+                continue
+            if arr[index] < arr[index - 1]:
+                arr[index], arr[index - 1] = arr[index - 1], arr[index]
+                swapped = True
     return arr
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
-
+    size = len(arr)
+    output = [0] * size
+    count = [0] * 10
+    for i in range(0, size):
+        count[arr[i]] += 1
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+    i = size - 1
+    while i >= 0:
+        output[count[arr[i]] - 1] = arr[i]
+        count[arr[i]] -= 1
+        i -= 1
+    for i in range(0, size):
+        arr[i] = output[i]
     return arr
+
+data = [4, 2, 2, 8, 3, 3, 1]
+count_sort(data)
+print("Sorted Array in Ascending Order: ")
+print(data)
+    
